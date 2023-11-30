fa7bdf469e6c3182bf56a846c0161b06d25fe801
LifeAsDev <angelocraft551@gmail.com>
2023-11-30 18:00:01 -0400

    ng for,if,class,style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TestComponent],
+  imports: [CommonModule, RouterOutlet, TestComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: '.app-test',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.sass',
 })
 export class TestComponent {
+  search = 'yo';
   public myId = 'niceID';
   public name = 'yoo';
 }
